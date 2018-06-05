1d9f6745b0b2121f48451d6ecb875c0b2f8a3f15
lorenaquint <lorenaquint@gmail.com>
2018-06-04 20:42:26 -0500

    Eliminar

--- ListasDemo/ListasDemo/ViewModel/FrendViewModel.cs
+++ ListasDemo/ListasDemo/ViewModel/FrendViewModel.cs
@@ -6,38 +6,61 @@ namespace ListasDemo.ViewModel
 	using ListasDemo.Model;
 	using Xamarin.Forms;
 
-	public class FrendViewModel
+    public class FrendViewModel
     {
-		public Command SaveFriendCommand
-		{
-			get;
-			set;
-		}
-		public Friend FriendModel
-		{
-			get;
-			set;
-		}
-		private INavigation Navigation;
-		public FrendViewModel(INavigation navigation)
+
+        public Command SaveFriendCommand
+        {
+            get;
+            set;
+        }
+        public bool IsEnabled
+        {
+            get;
+            set;
+        }
+        public Command DeleteFriendCommand
+        {
+            get;
+            set;
+        }
+        public Friend FriendModel
+        {
+            get => _friendModel;
+            set => _friendModel = value;
+        }
+        private INavigation Navigation;
+        private Friend _friendModel;
+
+        public FrendViewModel(INavigation navigation)
         {
 
-			FriendModel = new Friend();
-			SaveFriendCommand = new Command(async () => await SaveFriend());
-			Navigation = navigation;
+            FriendModel = new Friend();
+            SaveFriendCommand = new Command(async () => await SaveFriend());
+            IsEnabled = false;
+            Navigation = navigation;
         }
-		public FrendViewModel(INavigation navigation, Friend friend)
-		{
-			FriendModel = friend;
-			SaveFriendCommand = new Command(async () => await SaveFriend());
+
+        private async Task DeleteFriend()
+        {
+            await App.Database.DeleteFriendAsync(FriendModel);
+            await Navigation.PopToRootAsync();
+        }
+
+        public FrendViewModel(INavigation navigation, Friend friend)
+        {
+            FriendModel = friend;
+            IsEnabled = true;
+            SaveFriendCommand = new Command(async () => await SaveFriend());
+            DeleteFriendCommand = new Command(async () => await DeleteFriend());
             Navigation = navigation;
 
-		}
+        }
 
         public async Task SaveFriend()
-		{
-			await App.Database.SaveFriendAsync(FriendModel);
-			await Navigation.PopToRootAsync();
-		}
+        {
+            await App.Database.SaveFriendAsync(FriendModel);
+            await Navigation.PopToRootAsync();
+        }
     }
 }
